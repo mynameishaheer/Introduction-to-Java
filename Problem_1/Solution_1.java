3a0715cba8bd60fac6bf20a1fcb58a5c4f80f377
Shaheer Ahmed <94866222+mynameishaheer@users.noreply.github.com>
2022-01-04 22:25:48 +0500

    Uploaded Solution_1.1

---
+++ Problem_1/Solution_1.java
@@ -0,0 +1,12 @@
+public class Solution_1
+{
+	public static void main (String[] args)
+	{
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int c = a + b;
+		int d = a - b;
+		System.out.println("Output " + c + " and "+ d);
+		
+	}
+}
