081128bf57ced4addd6e42a64d54566fd6a3472b
Shaheer Ahmed <94866222+mynameishaheer@users.noreply.github.com>
2022-01-22 21:04:34 +0500

    Add files via upload

---
+++ Problem_11/ScannerEx.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ScannerEx {
+	public static void main(String[] args)  {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter your name: ");
+		String name = sc.nextLine();
+		
+		System.out.print("Enter your age: ");
+		int age = Integer.parseInt( sc.next() );
+		
+		System.out.println("Name: " + name);
+		System.out.println("Age: " + age);
+		
+		
+		
+		
+	}
+	
+}
